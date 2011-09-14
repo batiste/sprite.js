08a673bb3b6b6d1c84eca8f090dddb270308b333
Batiste Bieler <batiste.bieler@gmail.com>
2011-09-14 14:11:15 +0200

    Move extra features out of the core sprite.js file.

--- sprite.js
+++ sprite.js
@@ -31,7 +31,7 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * If you contribute don't forget to add your name in the AUTHORS file.
  */
 
-(function(global, undefined){
+(function(global){
 
 var doc = global.document;
 
@@ -42,10 +42,8 @@ var sjs = {
     SpriteList:SpriteList,
     Sprite:_Sprite,
     overlay:overlay,
-    SrollingSurface:SrollingSurface,
     scenes:[],
     math:{hypo:hypo, mod:mod, normalVector:normalVector},
-    path:{find:find_path}
 };
 
 // math function
@@ -1493,213 +1491,6 @@ SpriteList.prototype.iterate = function iterate() {
         return false;
     }
     return this.list[this.index];
-}
-
-function SrollingSurface(scene, w, h, redrawCallback) {
-
-    if(this.constructor !== arguments.callee)
-        return new SrollingSurface(scene, w, h, redrawCallback);
-
-    this.redrawCallback = redrawCallback;
-    this.block_h = Math.ceil(h / 2.0);
-    this.block_w = Math.ceil(w / 2.0);
-    this.x = 0;
-    this.y = 0;
-    this.w = w;
-    this.h = h;
-    this.scene = scene;
-    this.dom = doc.createElement('div');
-    this.dom.style.position = "relative";
-    scene.dom.appendChild(this.dom);
-
-    this.bufferCanvas = scene.Layer("buffer-"+Math.random(),
-            {w:1.5*this.w, h:1.5*this.h, x:0, y:0, autoClear:false,
-            useCanvas:true, parent:this.dom, disableAutoZIndex:true});
-
-    this.callbackCanvas = scene.Layer("buffer-"+Math.random(),
-            {w:this.block_w , h:this.block_h, x:0, y:0, autoClear:false,
-            useCanvas:true, parent:this.dom, disableAutoZIndex:true});
-
-    this.bufferCanvas.dom.style.display = 'none';
-    this.callbackCanvas.dom.style.display = 'none';
-
-    this.front = scene.Layer("front-"+Math.random(),
-        {w:this.w, h:this.h, x:0, y:0, autoClear:false,
-        useCanvas:true, parent:this.dom, disableAutoZIndex:true});
-
-    // block actually in use
-    this.rendered_blocks = [];
-    // layer available for rendering
-    this.available_blocks = [];
-}
-
-SrollingSurface.prototype.neededBlocks = function neededBlocks() {
-    // Return a list of needed block for the surface,
-    // the block is a int pair eg: (0,0), (10,12) that indicates the position
-    // (x / this.block_w, y / this.block_h) within the map.
-    var needed_blocks = [];
-    var x_block_start = Math.floor((this.x + (this.w / 2)) / this.block_w) -1;
-    var y_block_start = Math.floor((this.y + (this.h / 2)) / this.block_h) -1;
-
-    for(var x=0; x<3; x++) {
-        for(var y=0; y<3; y++) {
-            needed_blocks.push([x_block_start + x, y_block_start + y])
-        }
-    }
-    return needed_blocks
-}
-
-SrollingSurface.prototype.blockToRender = function blockToRender() {
-    // Return the blocks that need rendering
-    var neededBlocks = this.neededBlocks();
-    var toRender = [];
-    var rendered = this.rendered_blocks;
-    for(var i=0; i<neededBlocks.length; i++) {
-        var needed_block =  neededBlocks[i];
-        var found = false;
-        for(var j=0; j<rendered.length; j++) {
-            if(rendered[j].block[0] == needed_block[0] && rendered[j].block[1] == needed_block[1]) {
-                found = true;
-                break;
-            }
-        }
-        if(!found)
-            toRender.push(needed_block);
-    }
-    return toRender
-}
-
-SrollingSurface.prototype.renderBlocks = function renderBlocks() {
-    // render blocks in the buffer.
-
-    // new blocks to that need rendering
-    var render_list = this.blockToRender();
-    for(var i=0; i<render_list.length; i++) {
-        var toRender = render_list[i];
-        // real map coordinates
-        var x = toRender[0] * this.block_w;
-        var y = toRender[1] * this.block_h;
-
-        this.redrawCallback(this.callbackCanvas, x, y);
-        // buffer position in pixels
-        var buffer_pos = [
-            mod(toRender[0], 3) * this.block_w,
-            mod(toRender[1], 3) * this.block_h
-        ];
-
-        this.bufferCanvas.ctx.drawImage(this.callbackCanvas.dom, buffer_pos[0], buffer_pos[1]);
-
-        this.rendered_blocks.push({block:[toRender[0], toRender[1]], pos:[x, y], buffer_pos:buffer_pos});
-    }
-}
-
-SrollingSurface.prototype.recomposeBlocks = function recomposeBlocks() {
-    // draw all the blocks on the surface. My tests show that it's more efficient
-    // than redrawing the canvas on itself.
-    for(var i=0; i<this.rendered_blocks.length; i++) {
-        var block = this.rendered_blocks[i];
-        // draw the block on the front canvas
-        this.front.ctx.drawImage(this.bufferCanvas.dom,
-            block.buffer_pos[0], block.buffer_pos[1],
-            this.block_w, this.block_h,
-            block.pos[0] - this.x, block.pos[1] - this.y,
-            this.block_w, this.block_h);
-    }
-}
-
-SrollingSurface.prototype.move = function move(x, y) {
-    this.x = this.x + x;
-    this.y = this.y + y;
-    return this;
-}
-
-SrollingSurface.prototype.position = function position(x, y) {
-    this.x = x;
-    this.y = y;
-    return this;
-}
-
-SrollingSurface.prototype.deleteBlocks = function moveBlocks() {
-    for(var i=0; i<this.rendered_blocks.length; i++) {
-        var block = this.rendered_blocks[i];
-        if(
-            (block.pos[0] + this.block_w) < this.x ||
-            (block.pos[1] + this.block_h) < this.y ||
-            block.pos[0] > this.x + (this.w / 2) + this.block_w  ||
-            block.pos[1] > this.y + (this.h / 2) + this.block_h
-        ) {
-            delete block;
-            this.rendered_blocks.splice(i, 1);
-            i = i-1;
-        }
-    }
-}
-
-SrollingSurface.prototype.update = function update() {
-    this.deleteBlocks();
-    this.renderBlocks();
-    this.recomposeBlocks();
-}
-
-function find_path(startNode, endNode, maxVisit) {
-    if(maxVisit == undefined)
-        maxVisit = 1000;
-
-    // we start to search from the end so the
-    // final node is the start
-    endNode.cost = 0;
-    endNode.distance_to_start = endNode.distance(startNode);
-    var to_visit = [endNode];
-    var visited = [];
-    var current_node = false;
-    while(to_visit.length) {
-        current_node = to_visit[0];
-
-        // discard the node
-        if(current_node.disabled()) {
-            to_visit.shift();
-            continue;
-        }
-
-        // to avoid infinite search
-        if(visited.length > maxVisit) {
-            break
-        }
-
-        if(current_node.equals(startNode)) {
-            return current_node;
-        }
-
-        // check if the node is not already visited
-        var already_visited = false;
-        for(var i=0; i<visited.length; i++) {
-            if(visited[i].equals(current_node)) {
-                already_visited = true;
-                break;
-            }
-        }
-        if(already_visited) {
-            to_visit.shift();
-            continue;
-        }
-
-        visited.push(current_node);
-        to_visit.shift();
-
-        // add neighbors nodes
-        var neighbors = current_node.neighbors();
-        for(var i=0; i<neighbors.length; i++) {
-            neighbors[i].distance_to_start = endNode.distance(neighbors[i]);
-            neighbors[i].cost = neighbors[i].parent.cost + 1;
-        }
-        to_visit.push.apply(to_visit, neighbors);
-
-        // put the best candidate on top
-        to_visit.sort(function(a, b) {
-            // Less than 0: Sort "a" to be a lower index than "b", so on top of the list
-            return (a.cost + a.distance_to_start) - (b.cost + b.distance_to_start);
-        });
-    }
 }
 
 global.sjs = sjs;
